0d531109773caa73d73a9c1917bee9df454e6432
faceless00 <faceless0230@gmail.com>
2022-04-18 00:30:52 +0800

    part 1 fin

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+InputBuffer* new_input_buffer() {
+    InputBuffer* input_buffer = new InputBuffer;
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+    return input_buffer;
+}
+
+void print_prompt() { cout << "db > "; }
+
+void read_input(InputBuffer* input_buffer) {
+    ssize_t bytes_read =
+        getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+    if (bytes_read <= 0) {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);  // define EXIT_FAILURE 1
+    }
+
+    // 忽略换行符
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] =
+        0;  // c语言字符串结束标志为'\0',数值为0
+}
+
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);  //显然,是用堆分配的空间
+    free(input_buffer);
+}
+
+int main(int argc, char* argv[]) {
+    InputBuffer* input_buffer = new_input_buffer();
+    while (true) {
+        print_prompt();
+        read_input(input_buffer);
+
+        if (strcmp(input_buffer->buffer, ".exit") == 0) {
+            cout << "byebye~~" << endl;
+            close_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        } else {
+            cout << "Unrecognized command: " << input_buffer->buffer << endl;
+        }
+    }
+    return 0;
+}
